d93888b02f91829c9ad4efa53dd27c3a5b34ecf4
MiguelVasquezR <mvasquesrosas@gmail.com>
2025-05-05 13:09:48 -0600

    fix

--- src/pages/Invitacion.jsx
+++ src/pages/Invitacion.jsx
@@ -284,7 +284,9 @@ const Invitacion = () => {
                 <p className="text-lg font-semibold font-italiana">
                   Tu nombre es:
                 </p>
-                <p className="text-xl font-bold text-primary">Miguel Vásquez</p>
+                <p className="text-xl font-bold text-primary">
+                  {persona.nombre}
+                </p>
               </div>
 
               <div className="space-y-1">
@@ -336,6 +338,14 @@ const Invitacion = () => {
                   <div className="peer rounded-full outline-none duration-100 after:duration-500 w-28 h-14 bg-primary peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-primary/50 after:content-['No'] after:absolute after:outline-none after:rounded-full after:h-12 after:w-12 after:bg-white after:top-1 after:left-1 after:flex after:justify-center after:items-center after:text-sky-800 after:font-bold peer-checked:after:translate-x-14 peer-checked:after:content-['Sí'] peer-checked:after:border-white"></div>
                 </label>
               </div>
+
+              <a
+                href=".me/5212288538444?text=Hola, ame. Tengo una duda sobre los 15 años de tu hija ☺️"
+                className="underline text-blue-500 my-5 flex justify-center items-center"
+                target="_blank"
+              >
+                Cualquier duda, contacta a mi mamá ☺️
+              </a>
 
               <button
                 onClick={(e) => {
